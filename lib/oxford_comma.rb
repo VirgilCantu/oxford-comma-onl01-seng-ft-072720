63a141f8f122278b201dd786cccb819ecc35a8d0
Virgil Cantu <cantu.sampsom@gmail.com>
2020-07-29 19:45:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
 def oxford_comma(array)
+array.each |fruit|
 
-end
+
+
+end  
+
+
+
+
+
+
+
+
+
+## def oxford_comma(array)
+## if array.length == 1
+ ##  array.join
+## elsif array.length == 2  
+ ## array.join(" and ")
+## elsif array.length >= 3
+##  array[-2] << ", and"
+##  array.join(", ")
+ ## end
+##end
